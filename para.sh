1cbf5d63900fede1a9fa5a132679c3eeb65dda57
unixextremist <ysfmmekb@proton.me>
2026-02-22 18:49:49 +0400

    Update para.sh

--- para.sh
+++ para.sh
@@ -3,23 +3,23 @@
 command -v wmctrl >/dev/null 2>&1 || exit 1
 
 workspaces=$(
-   wmctrl -d | awk '
-       {
-           name = $NF
-           if (name == "-" || name == "") name = "ws" $1
-           if ($2 == "*") name = "[" name "]"
-           printf "%s%s", sep, name
-           sep = " / "
-       }
-   '
+    wmctrl -d | awk '
+        {
+            name = $NF
+            if (name == "-" || name == "") name = "ws" $1
+            if ($2 == "*") name = "[" name "]"
+            printf "%s%s", sep, name
+            sep = " / "
+        }
+    '
 )
 
 [ -z "$workspaces" ] && exit 1
 
 if command -v notify-send >/dev/null 2>&1; then
-   notify-send "Workspaces" "$workspaces"
+    notify-send "workspaces" "$workspaces"
 elif command -v herbe >/dev/null 2>&1; then
-   herbe "Workspaces: $workspaces"
+    herbe "workspaces: $workspaces"
 else
-   printf '%s\n' "$workspaces"
+    printf '%s\n' "$workspaces"
 fi
